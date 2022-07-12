ac9a70f609bed8a61b259aa61af305bd1e334273
nelson m <nelson.mori.proyectos@gmail.com>
2022-07-12 18:38:30 -0400

    se agrego scaffold de pet_history

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,22 @@
 class Pet < ApplicationRecord
+    def history_count
+        #TODO-implement
+      end
+    
+      def avg_weight
+        #TODO-implement
+      end
+    
+      def avg_height
+        #TODO-implement
+      end
+    
+      def max_weight
+        #TODO-implement
+      end
+    
+      def max_height
+        #TODO-implement
+      end
+    
 end
